7e4ca9201d48e98473c08e04a236137865819bda
tj.boberek@gmail.com <tj.boberek@gmail.com@1c4857e6-0069-bf53-7576-911745cc2654>
2010-07-13 00:09:35 +0000

    Adding basic booleans - and, or, not

---
+++ linkedlisp/src/main/java/com/googlecode/linkedlisp/functions/Not.java
@@ -0,0 +1,34 @@
+package com.googlecode.linkedlisp.functions;
+
+import com.googlecode.linkedlisp.Function;
+import com.googlecode.linkedlisp.ListExpression;
+import com.googlecode.linkedlisp.Expression;
+import com.googlecode.linkedlisp.State;
+
+public class Not extends Function {
+	public Not() {
+	}
+
+	@Override
+	public Object execute(State s, ListExpression params) throws Exception {
+		Object result = null;
+
+		// Not only takes on parameter!
+		if (params.size() > 1) {
+			throw new Exception();
+		}
+
+		if (null == params.get(0).evaluate(s)) {
+			result = true;
+		}
+
+		// Return our calculated value
+		return result;
+	}
+
+	@Override
+	public Object getValue() {
+		return "not";
+	}
+}
+
