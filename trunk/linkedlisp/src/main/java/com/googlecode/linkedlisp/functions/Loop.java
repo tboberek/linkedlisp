8160a1714afee4a1ef83296fa9a210a3c8ceb1cc
tj.boberek@gmail.com <tj.boberek@gmail.com@1c4857e6-0069-bf53-7576-911745cc2654>
2010-07-17 04:18:50 +0000

    Initial implementation of loop.

--- trunk/linkedlisp/src/main/java/com/googlecode/linkedlisp/functions/Loop.java
+++ trunk/linkedlisp/src/main/java/com/googlecode/linkedlisp/functions/Loop.java
@@ -1,28 +1,27 @@
 package com.googlecode.linkedlisp.functions;
 
-import java.util.Arrays;
-
 import com.googlecode.linkedlisp.Function;
 import com.googlecode.linkedlisp.ListExpression;
-import com.googlecode.linkedlisp.NoReturnException;
+import com.googlecode.linkedlisp.Expression;
 import com.googlecode.linkedlisp.State;
 
 public class Loop extends Function {
 
-    public Loop() {
-        setParameterNames(Arrays.asList(new String[]{"name","x", "y"}));
-    }
-    
-    private ListExpression params;
-
-    public void setParameters(ListExpression list) {
-        params = list;
-    }
-
     @Override
-	public Object execute(State s, ListExpression params) throws NoReturnException {
-        // TODO implement it.
-        return null;
+	public Object execute(State s, ListExpression params) throws Exception {
+		Object lastResult = null;
+
+		// If the first result is not null, continue on
+		// and evaluate all the other parameters
+		while (null != params.get(0).evaluate(s))
+		{
+			for (int i = 1; i < params.size(); i++)
+			{
+				lastResult = params.get(i).evaluate(s);
+			}
+        }
+
+		return lastResult;
     }
 
     @Override
