e26a4e0b85c61af7093f87005bdffca00b755d08
tj.boberek@gmail.com <tj.boberek@gmail.com@1c4857e6-0069-bf53-7576-911745cc2654>
2010-08-01 16:19:18 +0000

    Updating code to work with latest framework changes.

--- trunk/linkedlisp/src/main/java/com/googlecode/linkedlisp/functions/control/Loop.java
+++ trunk/linkedlisp/src/main/java/com/googlecode/linkedlisp/functions/control/Loop.java
@@ -7,22 +7,85 @@ import java.util.List;
 
 public class Loop extends Function {
 
+	private String blockName = null;
+
     @Override
-	public Object execute(Environment s, List params) throws Exception {
-		Object lastResult = null;
+	public Object execute(Environment s, List params) throws Exception 
+   	{
+		// Get the first string in our expression
+		String firstParam = s.evaluate(params.get(0)).toString();
 
-		// If the first result is not null, continue on
-		// and evaluate all the other parameters
-		while (null != s.evaluate(params.get(0)))
+		// Determine what form of loop we have
+		if (firstParam.equals("named"))
 		{
-			for (int i = 1; i < params.size(); i++)
+			// If we find a block after we already have one,
+			// then throw an exception
+			if (blockName != null)
 			{
-				lastResult = s.evaluate(params.get(i));
+				throw new Exception("named clause already found");
 			}
-        }
 
-		return lastResult;
+			// Get the name for the block
+			blockName = s.evaluate(params.get(1)).toString();
+
+			// Get the remaining expressions after we clear
+			// out our named clause
+			List remaining = params.subList (2, params.size());
+
+			// Execute with the remaining 
+			return execute (s, remaining);
+			}
+		else if (firstParam.equals("for"))
+		{
+			// The next part of our clause is the control
+			// variable
+			Object controlVar = params.get(1);
+
+			// After the control variable, we need to grab
+			// the control subclause
+			Object subclause = params.get(2);
+			
+			if (subclause.toString().equals("from"))
+			{
+			//	Integer intialValue = Integer.params.get(3).getValue
+			}
+			return null;
+		}
+		else
+		{
+			// Short form, only allowed if we haven't
+			// found any other clause (including a named
+			// clause)
+			if (blockName != null)
+			{
+				throw new 
+					Exception("found expression where LOOP keyword expected.");
+			}	
+
+			return ShortForm (s, params);
+		}
     }
+
+	private Object ShortForm (Environment s, List params) throws Exception
+	{
+		// Short-form loop - simply repeates forever
+		try 
+		{
+			while (true)
+			{
+				for (Object exp : params)
+				{
+				s.evaluate(exp);	
+				}
+			}
+		}
+		catch (Exception e)
+		{
+
+		}
+
+		return null;
+	}	
 
     @Override
     public Object getValue() {
