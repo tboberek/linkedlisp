429a37715cfc21910ca799b9bd74f96d654b849e
Pawel.Micun <Pawel.Micun@1c4857e6-0069-bf53-7576-911745cc2654>
2010-08-03 06:25:37 +0000

    a simple "run all the stuff in /tests" test

--- trunk/linkedlisp/src/test/java/com/googlecode/linkedlisp/AppTest.java
+++ trunk/linkedlisp/src/test/java/com/googlecode/linkedlisp/AppTest.java
@@ -1,38 +1,64 @@
 package com.googlecode.linkedlisp;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	static class TestInfo {
+		File filename;
+		Exception exception;
+		
+		TestInfo(File fileName, Exception exception) {
+			this.filename = fileName;
+			this.exception = exception;
+		}
+		
+		@Override
+		public String toString() {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			
+			exception.printStackTrace(new PrintWriter(out, true));
+			return filename + ": " + out.toString();
+		}
+	}
+	
+	@Test
+	public void runSamples() throws Exception {
+		// simply try running all the *.lisp in /tests
+	
+		List<TestInfo> failed = new LinkedList<TestInfo>();
+		
+		for(File f : new File("./tests").listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				return pathname.getName().endsWith(".lisp")
+					&& !pathname.getName().endsWith("LoopDoTest.lisp")
+					&& !pathname.getName().endsWith("LoopTest.lisp");
+			}
+		})) {
+			System.err.println("running " + f);
+			
+			try {
+				Main.main(new String[] {f.getAbsolutePath()});
+			} catch (Exception e) {
+				failed.add(new TestInfo(f, e));
+			}
+		}
+		
+		if(failed.size() > 0)
+			throw new RuntimeException(failed.size() + " tests failed: " + failed);
+		
+	}
 }
